92985f98466c3be8c7fed40162b8ea8b6ed4b151
psoto-go <pabletesglez@gmail.com>
2022-03-04 11:56:38 +0100

    first commit

---
+++ includes/philosophers.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: psoto-go <psoto-go@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/04 11:51:50 by psoto-go          #+#    #+#             */
+/*   Updated: 2022/03/04 11:55:44 by psoto-go         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef	PHILOSOPHERS_H
+# define PHILOSOPHERS_H
+# include <stdlib.h>
+# include <stdio.h>
+# include <string.h>
+# include <unistd.h>
+# include <stdarg.h>
+# include <limits.h>
+# include <fcntl.h>
+# include <unistd.h>
+# include <errno.h>
+
+
+#endif
